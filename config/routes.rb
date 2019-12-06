dd8fc109fdf31f462ba4ddddeb76309556b18316
efrankbased <eric.frank@seattlecolleges.edu>
2019-12-06 10:12:09 -0800

    Real App
    Basic App.
    Write (Articles/comments) View, Destroy.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/Index'
 
-  resources :articles
-
+  resources :articles do
+  	resources :comments
+end 
   root 'welcome#Index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
